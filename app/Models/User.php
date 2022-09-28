5b839302b39fa33803857bc7e6801183eec65f79
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2022-09-28 17:11:00 -0300

    'commit'

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
     protected $fillable = ['username', 'email', 'password', 'birth_date'];
 }
